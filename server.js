5121302776895dd3c1b6f2f80435c83799bf5d17
sudiptokumarmondal <30864302+sudiptokumarmondal@users.noreply.github.com>
2017-08-24 16:12:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config={
     
     user:'sudiptomondal',
     database:'sudiptomondal',
-    host:'db.imad.hasura-app.io'
+    host:'db.imad.hasura-app.io',
     port:'5432',
     password:process.env.db_password
     
